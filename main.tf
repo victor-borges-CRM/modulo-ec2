aeb15d16cb67de0c11b9aa1030f2e509f89b4ee7
Victor-Borges-Silva <victorborges975@gmail.com>
2024-08-12 16:44:12 -0300

    ajuste de variavel

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_vpc" "vpc_padrao" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.tipo_instancia
-  iam_instance_profile   = aws_iam_instance_profile.perfil_ec2.name
+  iam_instance_profile   = var.iam_instance_profile
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
